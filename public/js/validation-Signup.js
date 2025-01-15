c01f959287df9bef28c6176c124fc5a7b86d3b04
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-01-15 22:22:19 +0100

    ajout de validation front end par regex

---
+++ public/js/validation-Signup.js
@@ -0,0 +1,41 @@
+document.querySelector("signupform").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.querySelector('input[name="username"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const password = document.querySelector('input[name="password"]').value;
+    const confirmPassword = document.querySelector(
+      'input[name="confirm_password"]'
+    ).value;
+
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!usernameRegex.test(username)) {
+      alert(
+        "Username must be 3-20 characters and can only contain letters, numbers, and underscores."
+      );
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      alert("Changer cet email par un autre bien valider ✉️");
+      return;
+    }
+
+    if (!passwordRegex.test(password)) {
+      alert(
+        "Le mot de passe doit contenir au moins 8 caractères avec une lettre majuscule et un chiffre"
+      );
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas 🖐️");
+      return;
+    }
+
+    alert("Formulaire soumis avec succes ✅");
+    event.target.submit();
+  });
